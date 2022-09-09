7a7915d0d9530fe0914c2a5f301a191df498ade3
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-09-10 03:22:16 +0530

    added: show password and hide password state on Input fieeld

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,13 @@
-import { ChangeEvent, FC, HTMLInputTypeAttribute, ReactElement } from 'react';
+import {
+  ChangeEvent,
+  FC,
+  HTMLInputTypeAttribute,
+  ReactElement,
+  useState,
+} from 'react';
+import CloseEye from '../../icons/CloseEye';
+import OpenEye from '../../icons/OpenEye';
+import IconWrapper from '../IconWrapper';
 
 interface InputProps {
   id: string;
@@ -24,21 +33,43 @@ const Input: FC<InputProps> = ({
   displayName,
   error,
 }): ReactElement => {
+  const [inputType, setInputType] = useState(type);
+  const handleShow = (): void => {
+    if (type === 'password') {
+      setInputType((p) => (p === 'password' ? 'text' : 'password'));
+    }
+  };
   return (
     <div>
-      <label htmlFor={id} className="text-slate font-medium text-sm block mb-2">
+      <label className="text-slate font-medium text-sm block relative">
         {displayName}
+        <input
+          type={inputType}
+          id={id}
+          name={name}
+          className="bg-slate-50/10 text-medium p-3 border border-slate-50/10 rounded-lg w-full block focus:ring-2 outline-none  focus:ring-blue-500 mt-2"
+          placeholder={
+            type === 'password'
+              ? inputType === 'password'
+                ? placeholder
+                : 'StrongPass@1712'
+              : placeholder
+          }
+          required={required}
+          disabled={disabled}
+          onChange={onChange}
+        />
+        {type === 'password' && (
+          <div
+            onClick={handleShow}
+            className="cursor-pointer absolute right-2 top-1/2"
+          >
+            <IconWrapper>
+              {inputType === 'password' ? <OpenEye /> : <CloseEye />}
+            </IconWrapper>
+          </div>
+        )}
       </label>
-      <input
-        type={type}
-        id={id}
-        name={name}
-        className="bg-slate-50/10 text-medium p-3 border border-slate-50/10 rounded-lg w-full block focus:ring-2 outline-none  focus:ring-blue-500"
-        placeholder={placeholder}
-        required={required}
-        disabled={disabled}
-        onChange={onChange}
-      />
       {error !== null && (
         <span className="text-red-500 mt-2 text-sm">{error}</span>
       )}
