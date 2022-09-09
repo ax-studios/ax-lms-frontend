996f2be16d2990edee4ec767704128b46b02a62d
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-09-09 20:17:17 +0530

    changed Home page color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
-  <div className="font-bold text-9xl flex justify-center items-center w-screen h-screen">
+  <div className="font-bold text-9xl flex justify-center items-center w-screen h-screen bg-slate-900 text-slate-50">
     Ax Studios
   </div>
 );
