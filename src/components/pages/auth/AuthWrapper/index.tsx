87ec1697658c93dd8a43391cf917eb61cbbc6e7c
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-09-12 16:11:42 +0530

    restructed code

--- src/components/pages/auth/AuthWrapper/index.tsx
+++ src/components/pages/auth/AuthWrapper/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { FC, ReactElement } from 'react';
-import Logo from '../Logo';
+import { Logo } from '../../../../core';
 
 interface AuthWrapperProps {
   children: ReactElement;
---
+++ src/core/index.ts
@@ -0,0 +1,4 @@
+export { default as CTA } from './components/CTA';
+export { default as IconWrapper } from './components/IconWrapper';
+export { default as Input } from './components/Input';
+export { default as Logo } from './components/Logo';
