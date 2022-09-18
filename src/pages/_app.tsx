fe025b5ad4679910fccd996dd433e15d21cb338e
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-09-19 01:48:42 +0530

    added Usercontext to provide userData to components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from 'next/app';
 import { ReactNode } from 'react';
 import DashboardWrapper from '../components/pages/dashboard/DashboardWrapper';
+import { UserContext, UserData } from '../data/userData';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
   return (
-    <DashboardWrapper>
-      <Component {...pageProps} />
-    </DashboardWrapper>
+    <UserContext.Provider value={UserData}>
+      <DashboardWrapper>
+        <Component {...pageProps} />
+      </DashboardWrapper>
+    </UserContext.Provider>
   );
 }
 
