3da88b136533bf121dc1d3ab1079fbc4efbd8501
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-09-20 16:24:51 +0530

    added drawer witdth to config file

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,9 @@
 export const EMAIL_REGEX =
   /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 export const OTP_REGEX = /^\d{6}/;
+export const width = {
+  drawer: {
+    closed: '80px',
+    open: '320px',
+  },
+};
