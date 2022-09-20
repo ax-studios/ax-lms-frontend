0dbde82db7b1ef9aa8a5b421604fa64b03e75444
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-09-20 16:24:26 +0530

    added search Icon and fixes in Bell and Arrow

--- src/icons/ArrowIcon.tsx
+++ src/icons/ArrowIcon.tsx
@@ -3,8 +3,8 @@ import { FC } from 'react';
 const ArrowIcon: FC = () => {
   return (
     <svg
-      width="45"
-      height="45"
+      width="100%"
+      height="100%"
       viewBox="0 0 45 45"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
