bbb68ba52d7e22fce124aaef5b262b7bbfcd1a2f
Anna <ana84168@gmail.com>
2022-10-10 11:31:51 -0500

    fix map in render scope

--- src/components/pages/dashboard/Charts/attendancePie.tsx
+++ src/components/pages/dashboard/Charts/attendancePie.tsx
@@ -1,11 +1,10 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { Pie } from '@visx/shape';
 import { Group } from '@visx/group';
 import { Text } from '@visx/text';
 import attendance from '../../../../data/attendance';
 
 interface itemsInterface {
-  id: number;
   label: string;
   porcentage: number;
   color: string;
@@ -23,44 +22,46 @@ const AttendancePie: FC = () => {
     (acc, course) => acc + course.days_total,
     0
   );
+  const [items, setItems] = useState<itemsInterface[]>([]);
 
-  const pieItems: itemsInterface[] = [
-    {
-      id: 99,
-      label: 'Absent',
-      porcentage: (100 * (totalDays - totalDaysAttended)) / totalDays,
-      color: '#FFF',
-      font_color: '#EC485B',
-    },
-  ];
+  useEffect(() => {
+    const pieItems: itemsInterface[] = [
+      {
+        label: 'Absent',
+        porcentage: (100 * (totalDays - totalDaysAttended)) / totalDays,
+        color: '#FFF',
+        font_color: '#EC485B',
+      },
+    ];
 
-  attendance.map((item) => {
-    console.log(item);
-    pieItems.push({
-      id: item.id,
-      label: item.course,
-      porcentage: 100 * (item.days_attended / totalDays),
-      color: item.color,
-      font_color: '#fff',
+    attendance.map((item) => {
+      pieItems.push({
+        label: item.course,
+        porcentage: 100 * (item.days_attended / totalDays),
+        color: item.color,
+        font_color: '#fff',
+      });
+      return <></>;
     });
-    return <></>;
-  });
+
+    setItems(pieItems);
+  }, []);
 
   return (
     <svg width={width} height={width}>
       <Group top={half} left={half}>
         <Pie
-          data={pieItems}
+          data={items}
           pieValue={(data) => data.porcentage}
           outerRadius={half}
           innerRadius={half - 50}
           padAngle={0.01}
         >
           {(pie) => {
-            return pie.arcs.map((arc) => {
+            return pie.arcs.map((arc, index) => {
               const [centroidX, centroidY] = pie.path.centroid(arc);
               return (
-                <g key={arc.data.label}>
+                <g key={index}>
                   <path
                     d={pie.path(arc) as string}
                     fill={arc.data.color}
