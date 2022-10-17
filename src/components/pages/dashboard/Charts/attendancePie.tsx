7ac9446972d559ca74972665d6333a2f2ca95a77
Kest <arinchaik1994@gmail.com>
2022-10-17 09:18:25 +0700

    refactor state

--- src/components/pages/dashboard/Charts/attendancePie.tsx
+++ src/components/pages/dashboard/Charts/attendancePie.tsx
@@ -1,7 +1,7 @@
 import { Group } from '@visx/group';
 import { Pie } from '@visx/shape';
 import { Text } from '@visx/text';
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState, useRef } from 'react';
 import attendance from '../../../../data/attendance';
 
 // types
@@ -12,43 +12,53 @@ interface itemsInterface {
   font_color: string;
 }
 
+// Initial data
+const width = 260;
+const half = width / 2;
+const totalDaysAttended = attendance.reduce(
+  (acc, course) => acc + course.days_attended,
+  0
+);
+const totalDays = attendance.reduce(
+  (acc, course) => acc + course.days_total,
+  0
+);
+
 const AttendancePie: FC = () => {
-  const width = 260;
-  const half = width / 2;
   // Initial data
-  const totalDaysAttended = attendance.reduce(
-    (acc, course) => acc + course.days_attended,
-    0
-  );
-  const totalDays = attendance.reduce(
-    (acc, course) => acc + course.days_total,
-    0
-  );
-  const [items, setItems] = useState<itemsInterface[]>([]);
+  const [items, setItems] = useState<itemsInterface[]>([
+    {
+      label: 'Absent',
+      porcentage: (100 * (totalDays - totalDaysAttended)) / totalDays,
+      color: '#FFF',
+      font_color: '#EC485B',
+    },
+  ]);
+  const mount = useRef(true);
 
-  useEffect(() => {
-    // Initial data
-    const pieItems: itemsInterface[] = [
-      {
-        label: 'Absent',
-        porcentage: (100 * (totalDays - totalDaysAttended)) / totalDays,
-        color: '#FFF',
-        font_color: '#EC485B',
-      },
-    ];
-
-    attendance.map((item) => {
-      pieItems.push({
+  const fectchData = (): itemsInterface[] => {
+    const newPieValue: itemsInterface[] = [];
+    attendance.forEach((item) => {
+      newPieValue.push({
         label: item.course,
         porcentage: 100 * (item.days_attended / totalDays),
         color: item.color,
         font_color: '#fff',
       });
-      return <></>;
     });
+    return newPieValue;
+  };
 
-    setItems(pieItems);
-  }, [totalDays, totalDaysAttended]);
+  useEffect(() => {
+    if (mount.current) {
+      const response = fectchData();
+      setItems(response);
+    }
+
+    return () => {
+      mount.current = false;
+    };
+  }, []);
 
   return (
     <svg width={width} height={width}>
