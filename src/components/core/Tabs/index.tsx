32f719eecc7cbbf3c08bf538835a55328846ba17
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-10-28 00:54:09 +0530

    changes: tabs in user profile have better animation

--- src/components/core/Tabs/index.tsx
+++ src/components/core/Tabs/index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, FC, SetStateAction } from 'react';
+import { Dispatch, FC, SetStateAction, useState } from 'react';
 
 interface Props {
   tabs: Array<{ name: string; value: string }>;
@@ -7,18 +7,34 @@ interface Props {
 }
 
 const Tabs: FC<Props> = ({ tabs, value, setValue }) => {
+  const [width, setWidth] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [hover, setHover] = useState(false);
+
   return (
     <div className="flex justify-center">
-      <div className="flex gap-1 rounded-full bg-base-content/30 text-lg font-bold">
+      <div className="relative flex gap-1 rounded-lg border-2 border-secondary/30 bg-base-content/5 p-2 text-lg font-bold">
+        <div
+          className={`absolute top-0 z-0 h-[calc(100%-16px)] translate-y-2 rounded-lg bg-primary transition-all duration-300 ${
+            hover ? 'opacity-100' : 'opacity-0'
+          }`}
+          style={{ width, left }}
+        />
         {tabs.map((tab) => (
           <button
             key={tab.value}
-            className={`btn no-animation rounded-full px-3 py-2 ${
+            className={`btn z-10 rounded-none border-0 border-b-2 bg-transparent px-5 duration-300 hover:border-transparent hover:bg-transparent hover:text-base-300  ${
               tab.value === value
-                ? 'btn-primary border-0 hover:bg-primary'
-                : 'border-0 bg-transparent text-base-content hover:text-base-300'
+                ? 'border-primary text-base-content'
+                : 'border-transparent text-base-content/70'
             }`}
             onClick={() => setValue(tab.value)}
+            onMouseEnter={(e) => {
+              setWidth(e.currentTarget.offsetWidth);
+              setLeft(e.currentTarget.offsetLeft);
+              setHover(true);
+            }}
+            onMouseLeave={() => setHover(false)}
           >
             {tab.name}
           </button>
