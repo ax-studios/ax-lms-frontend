a0900552eef33f352d74b2da0d68af9f85b1fe7d
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-02 17:46:13 +0530

    removed dasiyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,14 @@
-const labelsClasses = ['indigo', 'gray', 'green', 'blue', 'red', 'purple'];
-const themes = [
-  // {
-  //   smile: {
-  //     primary: '#ff543d',
-  //     secondary: '#ffb522',
-  //     accent: '#53d7b7',
-  //     neutral: '#1D222A',
-  //     'base-100': '#f4f1ec',
-  //     info: '#88ACD7',
-  //     success: '#177D6A',
-  //     warning: '#CBA006',
-  //     error: '#E25336',
-  //   },
-  //   el_regaldo: {
-  //     primary: '#947cd3',
-  //     secondary: '#ef8a70',
-  //     accent: '#ce980e',
-  //     neutral: '#2E2532',
-  //     'base-100': '#242C47',
-  //     info: '#7FBADC',
-  //     success: '#19E6C7',
-  //     warning: '#C99B13',
-  //     error: '#F35862',
-  //   },
-  // },
-  // 'dark',
-  // 'winter',
-  // 'synthwave',
-  // 'halloween',
-  // 'cmyk',
-  // 'forest',
-  'night',
-  // 'wireframe',
-  // 'black',
-  // 'lofi',
-];
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  safelist: [
-    ...labelsClasses.map((lbl) => `bg-${lbl}-500`),
-    ...labelsClasses.map((lbl) => `bg-${lbl}-200`),
-    ...labelsClasses.map((lbl) => `text-${lbl}-400`),
-  ],
-
   theme: {
     extend: {
       keyframes: {
@@ -57,10 +23,15 @@ module.exports = {
       gridTemplateColumns: {
         '1/5': '1fr 5fr',
       },
+      colors: {
+        'base-content': withOpacity('--base-content'),
+        primary: withOpacity('--primary'),
+        'primary-focus': withOpacity('--primary-focus'),
+        'primary-accent': withOpacity('--primary-accent'),
+        'base-100': withOpacity('--base-100'),
+        'base-200': withOpacity('--base-200'),
+        'base-300': withOpacity('--base-300'),
+      },
     },
-  },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes,
   },
 };
